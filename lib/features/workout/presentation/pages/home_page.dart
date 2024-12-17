f433e296ec8cb3c704846a18a44122413f289b5c
DouglasQuashie <idotetudes@gmail.com>
2024-12-17 16:28:28 +0100

    ajout page profil

--- lib/features/workout/presentation/pages/home_page.dart
+++ lib/features/workout/presentation/pages/home_page.dart
@@ -1,6 +1,8 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:iworkout/features/workout/presentation/screens/home_screen.dart';
+import 'package:iworkout/features/workout/presentation/screens/profile_screen.dart';
+
 import 'package:iworkout/features/workout/presentation/screens/progress_screen.dart';
 import 'package:iworkout/features/workout/presentation/screens/workout_screen.dart';
 import 'package:iworkout/features/workout/presentation/widgets/floating_bottom_navigation_bar.dart';
@@ -15,10 +17,13 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   int _currentIndex = 0;
-  final List<Widget> _children = [
-    const HomeScreen(),
-    const WorkoutScreen(),
-    const ProgressScreen(),
+  final List<Widget> _children = const [
+     HomeScreen(),
+     WorkoutScreen(),
+     ProgressScreen(),
+    //ajout profil
+     ProfileScreen(),
+
   ];
 
   void onItemTapped(int index) {
